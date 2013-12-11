ef0c542ce492c0ce23835f6ffbd7676f082209c9
Piotr <Piotr@Piotr-Komputer>
2013-12-11 17:40:10 +0100

    Pierwszy commit zadania nr 2

---
+++ src/main/java/com/mycompany/zad2/controller.java
@@ -0,0 +1,20 @@
+package com.mycompany.zad2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.mycompany.zad2.model;
+@Controller
+@RequestMapping("/api/hello")
+public class controller { 
+    @RequestMapping(value="{person}",method= RequestMethod.GET)
+    public @ResponseBody model controller(@PathVariable String person) {
+        
+        model m = new model();
+        m.setPerson(person);
+        
+        return m;
+        
+    }
+}
---
+++ src/main/java/com/mycompany/zad2/model.java
@@ -0,0 +1,15 @@
+package com.mycompany.zad2;
+
+public class model {
+    String person;
+
+    public String getPerson() {
+        return "Witaj,"+person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+    
+    
+}
