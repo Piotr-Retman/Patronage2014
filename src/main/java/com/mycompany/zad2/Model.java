8ace90b0c0a2580a651e921152fb16a64ccf8efa
Piotr <Piotr@Piotr-Komputer>
2013-12-12 17:57:29 +0100

    Większe zmiany w organizacji plików i ich zawartości cz.6.

---
+++ src/main/java/com/mycompany/zad2/Model.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.zad2;
+
+public class Model {
+    private String person;
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+    
+}
