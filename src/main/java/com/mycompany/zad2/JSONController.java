82f3b7c2cc202be41ee6ecb2ff2883f8a8b52e80
Piotr <Piotr@Piotr-Komputer>
2013-12-12 18:28:50 +0100

    Większe zmiany w organizacji plików i ich zawartości cz.7.

--- src/main/java/com/mycompany/zad2/JSONController.java
+++ src/main/java/com/mycompany/zad2/JSONController.java
@@ -1,15 +1,16 @@
 package com.mycompany.zad2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.mycompany.zad2.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/api/hello")
 public class JSONController { 
     @RequestMapping(value="{person}",method= RequestMethod.GET)
-    public @ResponseBody Model jsoncontrol(@PathVariable("{person}") String person) {
+    public @ResponseBody Model jsoncontrol(@RequestParam("person") String person) {
         
         Model m = new Model();
         m.setPerson(person);
