2fde6ddff1f5f02c37df81c83f4722b13bbfa3bd
Piotr <Piotr@Piotr-Komputer>
2013-12-13 02:34:07 +0100

    Zmiany w package controllera i modelu.

--- src/main/java/com/mycompany/controller/JSONController.java
+++ src/main/java/com/mycompany/controller/JSONController.java
@@ -1,17 +1,15 @@
-package controller;
+package com.mycompany.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.mycompany.zad2.Model;
-import com.mycompany.zad2.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/api/hello/person")
 public class JSONController {
-    @RequestMapping(value = "hello",method = RequestMethod.GET)
-    @ResponseBody
-    public Model jsoncontrol(@RequestParam String person) {
+    @RequestMapping(value = "person",method = RequestMethod.GET)
+    public @ResponseBody Model jsoncontrol(@RequestParam String person) {
         Model m = new Model();
         m.setPerson(person);
         return m;
