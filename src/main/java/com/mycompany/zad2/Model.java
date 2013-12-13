05eaadb7d0d436b60e3e65bb2837ec45548cc7d6
Piotr <Piotr@Piotr-Komputer>
2013-12-14 00:39:02 +0100

    Zmiany w controller

--- src/main/java/com/mycompany/zad2/Model.java
+++ src/main/java/com/mycompany/zad2/Model.java
@@ -1,7 +1,11 @@
 package com.mycompany.zad2;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 public class Model {
     
     private String person;
+    @ModelAttribute
     public void setPerson(String person) {
         this.person = person;
     }
