d5775ecc508b682a90bb63ba952b163c4f443b94
Piotr <Piotr@Piotr-Komputer>
2013-12-13 20:31:19 +0100

    Zmiany w controller,dispatcher-servlet

--- src/main/java/com/mycompany/controller/JSONController.java
+++ src/main/java/com/mycompany/controller/JSONController.java
@@ -4,15 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.mycompany.zad2.Model;
+import javax.servlet.http.HttpServletResponse;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/api/hello")
 public class JSONController {
-    @RequestMapping(value = "person",method = RequestMethod.GET)
-    public @ResponseBody Model jsoncontrol(@RequestParam String person) {
-        Model m = new Model();
-        m.setPerson(person);
-        return m;
+    @RequestMapping(value="{person}",method = RequestMethod.GET,produces = "application/json")
+    public @ResponseBody Model jsoncontrol(@RequestParam(value = "person") String person,Model model) {
+        
+        model.setPerson(person);
+        
+        return model;
         
     }
 }
