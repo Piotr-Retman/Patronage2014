27077a9285f76ff72495d2f0497b802525db275e
Piotr <Piotr@Piotr-Komputer>
2013-12-14 23:42:52 +0100

    Zwraca wartości :)

--- src/main/java/com/mycompany/controller/JSONController.java
+++ src/main/java/com/mycompany/controller/JSONController.java
@@ -7,12 +7,14 @@ import com.mycompany.zad2.Model;
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
-@RequestMapping("/person")
+//@RequestMapping("")
 public class JSONController {
-    @RequestMapping(value = "person", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody Object jsoncontrol(@RequestParam(value = "person") String person){
+            
             Model m = new Model();
-            m.setPerson(person);
+            
+            m.setPerson("Hello,"+person);
             return m;
     }
 }
